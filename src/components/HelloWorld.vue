64d3bd548457ac941a024ea890a07b59130863fc
Yuxuan Che <chey@oregonstate.edu>
2022-05-23 22:05:40 -0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <form v-on:submit.prevent="submit()">
+    <input
+      placeholder="Radius"
+      maxlength="15"
+      type="text"
+      v-model="formMess.radius"
+    />
+    <button type="submit" class="ui-button"><span>Submit</span></button>
+  </form>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import LocationSdk from "../commons/location-sdk";
+import axios from "axios";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "from-data",
+  data() {
+    return {
+      formMess: {
+        radius: "",
+        latitude: "",
+        longitude: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      axios({
+        baseURL: "http://localhost:3000/",
+        method: "get",
+        url: "main/getResult",
+        data: this.formMess,
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+  mounted: function () {
+    console.log("start to get location now.");
+    let self = this;
+    LocationSdk.getLocation({
+      success: (res) => {
+        self.formMess.latitude = res.latitude;
+        self.formMess.longitude = res.longitude;
+      },
+      error: (res) => {
+        console.log("failed to get location:" + res);
+      },
+    });
+  },
+};
+</script>
